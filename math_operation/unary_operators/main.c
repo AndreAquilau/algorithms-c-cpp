6bcbb2c723253fa6d2d293d39c5baa8c86375198
andre.aquilau <andre.aquilau@outlook.com>
2025-03-16 16:27:34 -0400

    operadores

--- math_operation/unary_operators/main.c
+++ math_operation/unary_operators/main.c
@@ -6,7 +6,7 @@ int main()
     int a = 0;
     int b = 0;
 
-    // Operacao de Incremento e Decremento em C
+    // Operadores unarios
     a++;
     b++;
 
@@ -54,6 +54,19 @@ int main()
 
     printf("%d\n", a);
     printf("%d\n", b);
+
+    // Operadores aritiméticos do C
+    int soma = 1 + 1;
+    float subtracao = 15 - 5;
+    double multiplicacao = 15 * 5;
+    float divisao = 10 / -2;
+    int modulo = 10 % 2;
+
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %f\n", subtracao);
+    printf("Multiplicacao: %lf\n", multiplicacao);
+    printf("Divisao: %lf\n", -divisao);
+    printf("Modulo: %d\n", modulo);
 
     return 0;
 }
