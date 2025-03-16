7dcc8a784c8903922c0fd76be422ffed4e446ac5
andre.aquilau <andre.aquilau@outlook.com>
2025-03-16 16:14:47 -0400

    operadores

---
+++ math_operation/increment_decrement_operation/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    // Operacao de Incremento e Decremento em C
+    a++;
+    b++;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a += 5;
+    b += 5;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a--;
+    b--;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a -= 5;
+    b -= 5;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a += 10;
+    b += 10;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a *= 2;
+    b *= 2;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a /= 2;
+    b /= 2;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    a %= 2;
+    b %= 2;
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    return 0;
+}
