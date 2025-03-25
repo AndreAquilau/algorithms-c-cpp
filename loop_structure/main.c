92d94c3180c7d23771b71e51cdf9e025819d592e
andre.aquilau <andre.aquilau@outlook.com>
2025-03-25 00:38:54 -0400

    Loops

--- loop_structure/main.c
+++ loop_structure/main.c
@@ -8,21 +8,46 @@ int main(char *args)
 
     printf("Let's go!\n");
 
-    /*
-    while (true)
-    {
-        printf("Seconds %ds \n", i);
-        i++;
-        sleep(1);
-    }
-    */
+    // while (true)
+    // {
+    //     printf("Seconds %ds \n", i);
+    //     i++;
+    //     sleep(1);
+    // }
 
-    do
+    // do
+    // {
+    //     printf("Seconds %ds \n", i);
+    //     i++;
+    //     sleep(1);
+    // } while (i < 10);
+
+    // for (int y = 0; y <= 100; y++)
+    // {
+    //     if ((y % 2) == 0)
+    //     {
+    //         printf("O número %d é par\n", y);
+    //     }
+    //     else
+    //     {
+    //         printf("O número %d é Impar\n", y);
+    //     }
+    // }
+
+    int y = 0;
+    double sum = 0.00, num = 0.00;
+
+    while (y < 10)
     {
-        printf("Seconds %ds \n", i);
-        i++;
-        sleep(1);
-    } while (i < 0);
+        printf("Digite o número %d para cáculo da média!\n", y + 1);
+        scanf("%lf", &num);
+
+        sum += num;
+
+        y++;
+    }
+
+    printf("A média dos alunos é %.2lf\n", sum / y);
 
     return 0;
 }
