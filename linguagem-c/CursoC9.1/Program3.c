35b1150cf2c213d5bf362e71ab27de483b7fec70
andre.aquilau <andre.aquilau@outlook.com>
2025-05-20 00:50:00 -0400

    Pararemtros

--- linguagem-c/CursoC9.1/Program3.c
+++ linguagem-c/CursoC9.1/Program3.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 struct Pessoa
 {
@@ -12,12 +13,12 @@ typedef struct Pessoa Pessoa;
 Pessoa *list_pessoas_v1(Pessoa list[], int length);
 Pessoa *list_pessoas_v2(Pessoa list[10], int length);
 Pessoa *list_pessoas_v3(Pessoa list[][10], int length1);
+int list_number(int count, ...);
 
 int main()
 {
     puts("Inicio!");
-    Pessoa pessoas[1] = {{"Andre", 25}};
-    list_pessoas_v1(pessoas, 1);
+    list_number(10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
     return 0;
 }
 
@@ -40,4 +41,17 @@ Pessoa *list_pessoas_v3(Pessoa list[][10], int length1)
     Pessoa *pessoas = malloc(sizeof(Pessoa) * length1);
 
     return pessoas;
+}
+
+int list_number(int count, ...)
+{
+    va_list args;
+    va_start(args, count);
+
+    for (int i = 0; i < count; i++)
+    {
+        printf("Indice: %d\n", i);
+    }
+
+    return 0;
 }
